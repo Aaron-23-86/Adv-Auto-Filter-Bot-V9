99e3eb16ab600d0f51204cdd6d3417146d54e6b7
Aaron-23-86 <88765008+Aaron-23-86@users.noreply.github.com>
2021-08-11 13:22:29 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -10,7 +10,7 @@ class Translation(object):
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@Termin_a_t_o_r
 </i>"""    
     
     HELP_TEXT = """
